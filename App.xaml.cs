b22b858be8751c3b844a058badf565802a468a17
KNKNT <huliganpasha573@gmail.com>
2026-01-25 16:09:11 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace QRcodeStorage
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
