8681232413535f74b33c9ed7223b4051128e7cc6
KNKNT <huliganpasha573@gmail.com>
2026-02-06 17:38:16 +0300

    null converter

--- Views/ShowProduct.xaml.cs
+++ Views/ShowProduct.xaml.cs
@@ -25,6 +25,7 @@ namespace QRcodeStorage.Pages
     {
         ShowProductModel showProductModel = new();
         Categories catigories = new();
+        DataView dataView = new();              
 
         public ShowProduct()
         {
@@ -33,19 +34,65 @@ namespace QRcodeStorage.Pages
             LoadTable();
         }
         private void btnUpdate_Click(object sender, RoutedEventArgs e) => LoadTable();
+        private void cbQrCode_SelectionChanged(object sender, SelectionChangedEventArgs e) => Search();
+        private void cbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e) => Search();
+        private void tbSearchName_TextChanged(object sender, TextChangedEventArgs e) => Search();
+        private void btnResetFilters_Click(object sender, RoutedEventArgs e)
+        {
+            tbSearchName.Clear();
+            cbCategory.SelectedIndex = 0;   
+            cbQrCode.SelectedIndex = 0;
+        }
 
         private void LoadTable()
         {
-            DataView dataView = new();
             dataView = showProductModel.LoadProduct();
             dgProducts.ItemsSource = dataView;
             tblRowCount.Text = dataView.Count.ToString();
         }
+
         private void LoadCategoriesComboBox()
         {
             var categories = showProductModel.LoadCategories().Select(c => (c.Id, c.Category)).ToList();
             catigories.LoadComboBoxes(cbCategory, categories, "Все категории");
         }
+
+        private void Search()
+        {
+            try
+            {
+                List<string> filters = new List<string>();
+
+                if (!string.IsNullOrWhiteSpace(tbSearchName.Text))
+                {
+                    filters.Add($"[Name] LIKE '%{tbSearchName.Text}%'");
+                }
+
+                if (cbCategory.SelectedIndex > 0)
+                {
+                    string selectedCategory = cbCategory.Text.ToString();
+                    filters.Add($"[Category] = '{selectedCategory}'");
+                }
+
+                if (cbQrCode.SelectedIndex > 0)
+                {
+                    bool hasQrCode = cbQrCode.SelectedIndex switch
+                    {
+                        1 => true,
+                        2 => false
+                    };
+                    filters.Add($"[Qr] = {hasQrCode}");
+                }
+
+                string finalFilter = string.Join(" AND ", filters);
+                dataView.RowFilter = finalFilter;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при фильтрации: {ex.Message}");
+            }
+        }
+
     }
 
     public class BooleanConverter : IValueConverter
@@ -59,6 +106,21 @@ namespace QRcodeStorage.Pages
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             System.Convert.ToBoolean(value);
+            return value;
+        }
+    }
+
+    public class NullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            value = string.IsNullOrEmpty(value.ToString()) ? "–" : value;
+            return value;
+        }
+
+        public object ConvertBack(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            value = value.ToString() == "–" ? null : value;
             return value;
         }
     }
